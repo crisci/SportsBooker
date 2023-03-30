ca4291580364799f6edb79a4be0f427202b29c72
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-03-30 20:28:46 +0200

    fix: statistics updated

---
+++ Lab_2/app/src/main/java/com/example/lab_2/ShowProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- Lab_2/app/src/test/java/com/example/lab_2/ExampleUnitTest.kt
+++ Lab_2/app/src/test/java/com/example/lab_2/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.lab2
+package com.example.lab_2
 
 import org.junit.Test
 
