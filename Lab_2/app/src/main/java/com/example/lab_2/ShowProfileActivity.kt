58b564a9eb8519e00c0e4a0b87b9de6d9b6f6bbb
crisci <crisciluigi2000@gmail.com>
2023-03-31 21:52:57 +0200

    menu edit

--- Lab_2/app/src/main/java/com/example/lab_2/ShowProfileActivity.kt
+++ Lab_2/app/src/main/java/com/example/lab_2/ShowProfileActivity.kt
@@ -2,10 +2,33 @@ package com.example.lab_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.AdapterView
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        //val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when (item.itemId) {
+            R.id.edit -> {
+                //TODO: Launch EditProfileActivity
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
 }
