44d5f5dcea21e25a059d51f797d72f412da5ce74
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-04-30 16:29:53 +0200

    fix: changed type converters for date and time, in order to visualize the date in the db in a readable way

--- Lab_3/app/src/main/java/com/example/lab2/database/ReservationAppDatabase.kt
+++ Lab_3/app/src/main/java/com/example/lab2/database/ReservationAppDatabase.kt
@@ -18,7 +18,7 @@ import com.example.lab2.database.player_reservation_join.PlayerReservationDAO
 import com.example.lab2.database.reservation.Reservation
 import com.example.lab2.database.reservation.ReservationDAO
 
-@Database(entities = [Player::class, Reservation::class, Court::class, PlayerBadgeRating::class, PlayerReservation::class], version = 2, exportSchema = false)
+@Database(entities = [Player::class, Reservation::class, Court::class, PlayerBadgeRating::class, PlayerReservation::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class ReservationAppDatabase : RoomDatabase() {
     abstract fun playerDao() : PlayerDAO
@@ -32,12 +32,6 @@ abstract class ReservationAppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: ReservationAppDatabase? = null
 
-
-        private val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE courts ADD COLUMN maxNumOfPlayers INTEGER DEFAULT 0 NOT NULL")
-            }
-        }
         fun getDatabase(context: Context) : ReservationAppDatabase =
             (INSTANCE ?:
             synchronized(this){
@@ -45,7 +39,7 @@ abstract class ReservationAppDatabase : RoomDatabase() {
                     context.applicationContext,
                     ReservationAppDatabase::class.java,
                     "sample.db"
-                ).createFromAsset("database/sample.db").addMigrations(MIGRATION_1_2).build()
+                ).createFromAsset("database/sample.db").build()
                 INSTANCE = i
                 INSTANCE
             })!!
