768171df8e956ead55652ee6e20a55532512bf16
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-05-26 11:22:17 +0200

    feat: login and signup authentication

--- Lab_5/app/src/main/java/com/example/lab2/FragmentLogin.kt
+++ Lab_5/app/src/main/java/com/example/lab2/FragmentLogin.kt
@@ -7,7 +7,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.example.lab2.calendar.MainVM
+import com.example.lab2.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FragmentLogin : Fragment(R.layout.fragment_login) {
@@ -16,7 +23,11 @@ class FragmentLogin : Fragment(R.layout.fragment_login) {
         fun newInstance() = FragmentLogin()
     }
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var viewModel: FragmentLoginVM
+    private lateinit var binding: FragmentLoginBinding
+    @Inject
+    lateinit var mainVM: MainVM
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +35,60 @@ class FragmentLogin : Fragment(R.layout.fragment_login) {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
 
+        binding = FragmentLoginBinding.bind(view)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         view.findViewById<View>(R.id.login).setOnClickListener {
-            val intent = Intent(requireActivity(), MyReservationsActivity::class.java)
-            startActivity(intent)
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    requireActivity(),
+                                    "Logged in successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                mainVM.setUser(firebaseAuth.uid!!)
+                                val intent = Intent(requireActivity(), MyReservationsActivity::class.java)
+                                startActivity(intent)
+
+                            } else {
+                                showValidationError(task.exception)
+                            }
+                        }
+            }
+            else {
+                Toast.makeText(requireActivity(), "Please fill in all fields", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
         return view
+    }
+
+    private fun showValidationError(exception: Exception?) {
+        if (exception is FirebaseAuthException) {
+            when(exception.errorCode) {
+                "ERROR_INVALID_EMAIL" -> binding.email.error = "Invalid email"
+                "ERROR_WRONG_PASSWORD" -> Toast.makeText(
+                    requireActivity(),
+                    "Wrong email and/or password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        else {
+            Toast.makeText(
+                requireActivity(),
+                "Error: ${exception?.message}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- Lab_5/app/src/main/java/com/example/lab2/FragmentSignup.kt
+++ Lab_5/app/src/main/java/com/example/lab2/FragmentSignup.kt
@@ -6,6 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.lab2.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,13 +20,65 @@ class FragmentSignup : Fragment(R.layout.fragment_signup) {
         fun newInstance() = FragmentSignup()
     }
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var viewModel: FragmentSignupViewModel
+    private lateinit var binding: FragmentSignupBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val view = inflater.inflate(R.layout.fragment_signup, container, false)
+        binding = FragmentSignupBinding.bind(view)
+
+        binding.signup.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val confirmPassword = binding.confirmPasswordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                if (password == confirmPassword) {
+                        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(
+                                        requireActivity(),
+                                        "Account created successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else {
+                                    showValidationError(task.exception)
+                                }
+                            }
+                } else {
+                    Toast.makeText(requireActivity(), "Passwords do not match", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(requireActivity(), "Please fill in all fields", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        return view
+    }
+
+    private fun showValidationError(exception: Exception?) {
+        if (exception is FirebaseAuthException) {
+            when(exception.errorCode) {
+                "ERROR_EMAIL_ALREADY_IN_USE" -> binding.email.error = "Email already in use"
+                "ERROR_WEAK_PASSWORD" -> binding.password.error = "Password must be at least 6 characters long"
+                "ERROR_INVALID_EMAIL" -> binding.email.error = "Invalid email"
+            }
+        } else {
+            Toast.makeText(
+                requireActivity(),
+                "Error: ${exception?.message}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
