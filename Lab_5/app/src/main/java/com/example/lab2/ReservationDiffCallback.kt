93e127bbd3b814e7995b61798bb38cf5bf008f37
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2023-05-30 00:43:58 +0200

    feat: update reservation diff callback

--- Lab_5/app/src/main/java/com/example/lab2/ReservationDiffCallback.kt
+++ Lab_5/app/src/main/java/com/example/lab2/ReservationDiffCallback.kt
@@ -6,8 +6,10 @@ import com.example.lab2.database.reservation.Reservation
 import com.example.lab2.database.reservation.ReservationWithCourt
 import com.example.lab2.database.reservation.ReservationWithCourtAndEquipments
 import com.example.lab2.database.reservation.formatPrice
+import com.example.lab2.viewmodels_firebase.Court
 import com.example.lab2.viewmodels_firebase.Match
 import com.example.lab2.viewmodels_firebase.MatchWithCourtAndEquipments
+import com.example.lab2.viewmodels_firebase.ReservationViewModel
 
 class ReservationDiffCallback(
     private  val reservations: List<MatchWithCourtAndEquipments>,
@@ -23,9 +25,31 @@ class ReservationDiffCallback(
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        val (_, courtId, numOfPlayers, price, date, time) = reservations[oldItemPosition].court
-        val (_, courtId1, numOfPlayers1, price1, date1, time1) = newReservations[newItemPosition].court
-        return  courtId == courtId1 && numOfPlayers == numOfPlayers1 && price == price1 && date == date1 && time == time1
+        return areCourtTheSame(reservations[oldItemPosition].court, newReservations[newItemPosition].court) &&
+                areMatchTheSame(reservations[oldItemPosition].match, newReservations[newItemPosition].match) &&
+                areReservationTheSame(reservations[oldItemPosition], newReservations[newItemPosition])
+
+    }
+
+    private fun areCourtTheSame(oldCourt: Court, newCourt: Court): Boolean {
+        return oldCourt.courtId == newCourt.courtId &&
+                oldCourt.description == newCourt.description &&
+                oldCourt.image == newCourt.image &&
+                oldCourt.maxNumberOfPlayers == newCourt.maxNumberOfPlayers &&
+                oldCourt.name == newCourt.name &&
+                oldCourt.sport == newCourt.sport
+    }
+
+    private fun areMatchTheSame(oldMatch: Match, newMatch: Match): Boolean {
+        return oldMatch.matchId == newMatch.matchId &&
+                oldMatch.date == newMatch.date &&
+                oldMatch.time == newMatch.time &&
+                oldMatch.numOfPlayers == newMatch.numOfPlayers
+    }
+
+    private fun areReservationTheSame(oldReservation: MatchWithCourtAndEquipments, newReservation: MatchWithCourtAndEquipments): Boolean {
+        return oldReservation.reservationId == newReservation.reservationId &&
+                oldReservation.finalPrice == newReservation.finalPrice
     }
 
 
